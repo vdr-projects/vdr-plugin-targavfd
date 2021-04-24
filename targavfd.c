36dda7933465fd556b3b0ec848923aebe15fbe87
Manuel Reimer <manuel.reimer@gmx.de>
2021-04-24 16:47:45 +0200

    Version 0.3.2

--- targavfd.c
+++ targavfd.c
@@ -3,8 +3,8 @@
  *
  * (C) 2010-2017 Andreas Brachold <vdr07 AT deltab de>
  *
- * This targavfd plugin is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as published 
+ * This targavfd plugin is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, version 3 of the License.
  *
  * See the files README and COPYING for details.
@@ -21,7 +21,7 @@
 #include "status.h"
 #include "setup.h"
 
-static const char *VERSION        = "0.3.1";
+static const char *VERSION        = "0.3.2";
 
 cPluginTargaVFD::cPluginTargaVFD(void)
 {
